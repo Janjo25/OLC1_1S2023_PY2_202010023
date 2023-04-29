a038911a5b3a1f3397d2a0a5fb1900e97e8878e1
Luis César Lizama Quiñónez <ElLuisLizama25@gmail.com>
2023-04-29 13:08:10 -0600

    refactor: remove line and column constructor parameters

--- back-end/interpreter/tables/symbols/interpreterSymbol.ts
+++ back-end/interpreter/tables/symbols/interpreterSymbol.ts
@@ -1,27 +1,14 @@
 class InterpreterSymbol {
-    private readonly _column: number;
-    private readonly _line: number;
     private readonly _name: string;
     private readonly _type: number;
     private readonly _value: null | number;
 
-    constructor(type: number, name: string, value: null | number, line: number, column: number) {
+    constructor(type: number, name: string, value: null | number) {
         this._type = type;
 
         this._name = name;
 
         this._value = value;
-
-        this._line = line;
-        this._column = column;
-    };
-
-    get column(): number {
-        return this._column;
-    };
-
-    get line(): number {
-        return this._line;
     };
 
     get name(): string {
