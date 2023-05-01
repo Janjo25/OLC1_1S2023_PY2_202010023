97cf2b0d612082b5c85c29b5602cbdee42c6c5e7
Luis César Lizama Quiñónez <ElLuisLizama25@gmail.com>
2023-05-01 16:50:59 -0600

    refactor: change "_identifier" from private to public

--- back-end/interpreter/expressions/identifier/identifier.ts
+++ back-end/interpreter/expressions/identifier/identifier.ts
@@ -3,22 +3,22 @@ import {Expression} from "../expression";
 import {InterpreterSymbol} from "../../tables/symbols/interpreterSymbol";
 
 class Identifier extends Expression {
-    private readonly _identifier: string;
+    readonly identifier: string;
 
     constructor(identifier: string) {
         super();
 
-        this._identifier = identifier;
+        this.identifier = identifier;
     };
 
     getType(environment: Environment): number | void {
-        const symbol: InterpreterSymbol | undefined = environment.getSymbol(this._identifier);
+        const symbol: InterpreterSymbol | undefined = environment.getSymbol(this.identifier);
 
         return (symbol !== undefined) ? symbol.type : alert("No se puede obtener información del tipo.");
     };
 
     getValue(environment: Environment): boolean | number | string | void {
-        const symbol: InterpreterSymbol | undefined = environment.getSymbol(this._identifier);
+        const symbol: InterpreterSymbol | undefined = environment.getSymbol(this.identifier);
 
         return (symbol !== undefined) ? symbol.value : alert("No se puede obtener información del valor.");
     };
